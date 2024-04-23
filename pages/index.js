91ab0a1f5d9709dd3555fecd447623c64fa9f923
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-22 18:05:35 -0700

    Footer updated.

--- pages/index.js
+++ pages/index.js
@@ -18,24 +18,24 @@ export default function Home() {
         </p>
 
         <section className={styles.plans}>
-        <Link href="/subscribe" passHref>
-          <article className={styles.card} role="button" tabIndex="0">
-            <h2>Monthly Plan - $4.99</h2>
-            <p>Includes 10 GlassGo. bags</p>
-            <p>Twice a week pickup</p>
-            <div className={styles.button}>Subscribe</div>
-          </article>
-        </Link>
+          <Link href="/subscribe" passHref>
+            <article className={styles.card} role="button" tabIndex="0">
+              <h2>Monthly Plan - $4.99</h2>
+              <p>Includes 10 GlassGo. bags</p>
+              <p>Twice a week pickup</p>
+              <div className={styles.button}>Subscribe</div>
+            </article>
+          </Link>
 
-        <Link href="/subscribe" passHref>
-          <article className={styles.card} role="button" tabIndex="0">
-            <h2>Premium Plan - $6.99</h2>
-            <p>Includes 12 GlassGo. bags</p>
-            <p>Three times a week pickup</p>
-            <div className={styles.button}>Subscribe</div>
-          </article>
-        </Link>
-      </section>
+          <Link href="/subscribe" passHref>
+            <article className={styles.card} role="button" tabIndex="0">
+              <h2>Premium Plan - $6.99</h2>
+              <p>Includes 12 GlassGo. bags</p>
+              <p>Three times a week pickup</p>
+              <div className={styles.button}>Subscribe</div>
+            </article>
+          </Link>
+        </section>
       </main>
 
       <footer className={styles.footer}>
