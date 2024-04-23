54cfb78085bbd894c770eb1661c964235e31f8b9
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-23 15:49:12 -0700

    Updated Header.module.css and Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 // pages/index.js
 
 import React from 'react';
-import Link from 'next/link'; // Import Link to wrap your subscribe buttons
-import Layout from '../components/Layout'; // Ensure the import path is correct
-import styles from '../styles/Home.module.css'; // Ensure the path to your styles is correct
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <Layout isHomepage={true}> {/* The isHomepage prop helps to set the header style */}
+    <Layout isHomepage={true}>
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>GlassGo.</h1>
@@ -21,6 +21,9 @@ export default function Home() {
                 <h2>Pick Twice Weekly - $5.99</h2>
                 <p>Includes 8 GlassGo. bags</p>
                 <p>Twice a week pickup</p>
+                <div className={styles.svgContainer} style={{ display: 'none' }}>
+                  <img src="/Bag.svg" alt="Bag Icon" />
+                </div>
                 <div className={styles.button}>Subscribe</div>
               </article>
             </Link>
@@ -30,6 +33,9 @@ export default function Home() {
                 <h2>Tri-Weekly Treasures - $7.99</h2>
                 <p>Become a premium to enjoy three time weekly pick ups, Includes 12 Monthly GlassGo. bags</p>
                 <p>Three times a week pickup</p>
+                <div className={styles.svgContainer} style={{ display: 'none' }}>
+                  <img src="/Bag.svg" alt="Bag Icon" />
+                </div>
                 <div className={styles.button}>Subscribe</div>
               </article>
             </Link>
