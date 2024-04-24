db75268fa63361ec9474ca0d4f140e3a3481393c
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-23 23:55:07 -0700

    Updated the heights of the Homepage, and Subs page css.

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -13,7 +13,7 @@ const Subscribe = () => {
           <img src="/Bag.svg" alt="Recycling Bag" className={styles.planImage} />
           <h2 className={styles.price}>Pick Twice Weekly - $5.99</h2>
           <p className={styles.description}>
-            Twice-weekly doorstep bottle pickup and 8 monthly bags for effortless recycling with GlassGo.
+            Twice-weekly doorstep bottle pickup + 8 monthly bags for effortless recycling with GlassGo.
           </p>
           <button
             className={styles.subscribeButton}
@@ -26,7 +26,7 @@ const Subscribe = () => {
           <img src="/Bag.svg" alt="Recycling Bag" className={styles.planImage} />
           <h2 className={styles.price}>Tri-Weekly Treasures - $7.99</h2>
           <p className={styles.description}>
-            Enjoy premium features with our best value plan. Three weekly pickups, 12 bags monthly.
+            Enjoy premium features with our best value plan. Three weekly pickups + 12 bags monthly.
           </p>
           <button
             className={styles.subscribeButton}
