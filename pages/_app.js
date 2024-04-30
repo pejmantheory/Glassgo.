c297a9ff718cc320ff3c98e8ad18d235195c23b9
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-30 02:59:59 -0700

    Clean _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-// pages/_app.js
-import '../styles/globals.css'; // Ensure this path correctly points to your global styles file.
+import '../styles/globals.css'; 
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
