48ce206c0f7571eeb855e2ec4faec0710214d38e
Pejman H. Nia <81389644+pejmantheory@users.noreply.github.com>
2024-06-06 04:13:59 -0700

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/Footer.module.css';  // Ensure the CSS module is correctly pointed
+import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
     return (
